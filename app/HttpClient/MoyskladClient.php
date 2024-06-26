667e32f7f1642e411607c357bdc557364b8d1190
Veten <vetenskap2@gmail.com>
2024-06-26 15:04:57 +0500

    save changes

--- app/HttpClient/MoyskladClient.php
+++ app/HttpClient/MoyskladClient.php
@@ -13,7 +13,7 @@ class MoyskladClient
 {
     public PendingRequest $request;
 
-    public function __construct(string $apiKey)
+    public function __construct(public string $apiKey)
     {
         $this->request = Http::retry(3, 2000, function (\Exception $exception, PendingRequest $request) {
             return ($exception instanceof RequestException && $exception->response->tooManyRequests()) || $exception instanceof ConnectionException;
@@ -21,7 +21,7 @@ class MoyskladClient
             ->timeout(60)
             ->connectTimeout(60)
             ->withHeaders([
-                'Authorization' => 'Bearer ' . $apiKey,
+                'Authorization' => 'Bearer ' . $this->apiKey,
                 'Accept-Encoding' => 'application/gzip',
                 'Content-Type' => 'application/json'
             ])->baseUrl('https://api.moysklad.ru/api/remap/1.2');
@@ -29,73 +29,118 @@ class MoyskladClient
 
     public function getWarehouses(): Collection
     {
-        while (RateLimiter::attempts('moysklad_api') >= 45) {
+        while (RateLimiter::attempts('moysklad_api_' . $this->apiKey) >= 45) {
             sleep(2);
         }
 
+        while (RateLimiter::attempts('moysklad_api_account_' . $this->apiKey) >= 5) {
+            sleep(1);
+        }
+
         return RateLimiter::attempt(
-            'moysklad_api',
-            45,
-            fn() => $this->request->get('/entity/store')->throw()->collect('rows'),
-            3
+            'moysklad_api_account_' . $this->apiKey,
+            5,
+            fn() => RateLimiter::attempt(
+                'moysklad_api_' . $this->apiKey,
+                45,
+                fn() => $this->request->get('/entity/store')->throw()->collect('rows'),
+                3
+            ),
+            1
         );
     }
 
     public function getWarehouseStocks(string $uuid): Collection
     {
-        while (RateLimiter::attempts('moysklad_api') >= 45) {
+        while (RateLimiter::attempts('moysklad_api_' . $this->apiKey) >= 45) {
             sleep(2);
         }
 
+        while (RateLimiter::attempts('moysklad_api_account_' . $this->apiKey) >= 5) {
+            sleep(1);
+        }
+
         return RateLimiter::attempt(
-            'moysklad_api',
-            45,
-            fn() => $this->request->withQueryParameters([
-                'filter' => 'store=https://api.moysklad.ru/api/remap/1.2/entity/store/' . $uuid
-            ])->get('/report/stock/all')->throw()->collect(),
-            3
+            'moysklad_api_account_' . $this->apiKey,
+            5,
+            fn() => RateLimiter::attempt(
+                'moysklad_api_' . $this->apiKey,
+                45,
+                fn() => $this->request->withQueryParameters([
+                    'filter' => 'store=https://api.moysklad.ru/api/remap/1.2/entity/store/' . $uuid
+                ])->get('/report/stock/all')->throw()->collect(),
+                3
+            ),
+            1
         );
     }
 
     public function getProductAttributes(): Collection
     {
-        while (RateLimiter::attempts('moysklad_api') >= 45) {
+        while (RateLimiter::attempts('moysklad_api_' . $this->apiKey) >= 45) {
             sleep(2);
         }
 
+        while (RateLimiter::attempts('moysklad_api_account_' . $this->apiKey) >= 5) {
+            sleep(1);
+        }
+
         return RateLimiter::attempt(
-            'moysklad_api',
-            45,
-            fn() => $this->request->get('/entity/product/metadata/attributes')->throw()->collect('rows'),
-            3
+            'moysklad_api_account_' . $this->apiKey,
+            5,
+            fn() => RateLimiter::attempt(
+                'moysklad_api_' . $this->apiKey,
+                45,
+                fn() => $this->request->get('/entity/product/metadata/attributes')->throw()->collect('rows'),
+                3
+            ),
+            1
         );
     }
 
     public function getSuppliers(): Collection
     {
-        while (RateLimiter::attempts('moysklad_api') >= 45) {
+        while (RateLimiter::attempts('moysklad_api_' . $this->apiKey) >= 45) {
             sleep(2);
         }
 
+        while (RateLimiter::attempts('moysklad_api_account_' . $this->apiKey) >= 5) {
+            sleep(1);
+        }
+
         return RateLimiter::attempt(
-            'moysklad_api',
-            45,
-            fn() => $this->request->get('/entity/counterparty')->throw()->collect('rows'),
-            3
+            'moysklad_api_account_' . $this->apiKey,
+            5,
+            fn() => RateLimiter::attempt(
+                'moysklad_api_' . $this->apiKey,
+                45,
+                fn() => $this->request->get('/entity/counterparty')->throw()->collect('rows'),
+                3
+            ),
+            1
         );
     }
 
     public function getAssortment(int $offset, int $limit): Collection
     {
-        while (RateLimiter::attempts('moysklad_api') >= 45) {
+        while (RateLimiter::attempts('moysklad_api_' . $this->apiKey) >= 45) {
             sleep(2);
         }
 
+        while (RateLimiter::attempts('moysklad_api_account_' . $this->apiKey) >= 5) {
+            sleep(1);
+        }
+
         return RateLimiter::attempt(
-            'moysklad_api',
-            45,
-            fn() => $this->request->get("/entity/assortment?limit=$limit&offset=$offset")->throw()->collect(),
-            3
+            'moysklad_api_account_' . $this->apiKey,
+            5,
+            fn() => RateLimiter::attempt(
+                'moysklad_api_' . $this->apiKey,
+                45,
+                fn() => $this->request->get("/entity/assortment?limit=$limit&offset=$offset")->throw()->collect(),
+                3
+            ),
+            1
         );
     }
 }
