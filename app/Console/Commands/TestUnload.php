c905e8345a41f67be03162a22db086f2903c5a42
Veten <vetenskap2@gmail.com>
2024-10-23 10:01:51 +0500

    test dispatch process data

---
+++ app/Console/Commands/TestUnload.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\EmailSupplier;
+use App\Services\EmailSupplierService;
+use Illuminate\Console\Command;
+
+class TestUnload extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-unload {emailSupplierId} {path}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $service = new EmailSupplierService(EmailSupplier::find($this->argument('emailSupplierId')), $this->argument('path'));
+        $service->unload();
+    }
+}
