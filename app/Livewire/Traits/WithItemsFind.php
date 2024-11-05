bc4f75519d72bd94f139964bfec97c2f5f73cd4d
Veten <vetenskap2@gmail.com>
2024-11-05 17:08:32 +0500

    fix with items find livewire trait

--- app/Livewire/Traits/WithItemsFind.php
+++ app/Livewire/Traits/WithItemsFind.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Traits;
 
+use App\Models\Item;
+
 trait WithItemsFind
 {
     public $searchItems;
@@ -13,7 +15,10 @@ trait WithItemsFind
         $this->items = $this->currentUser()
             ->items()
             ->when($this->searchItems, function ($query) {
-                $query->where('code', 'like', '%' . $this->searchItems . '%')->orWhere('name', 'like', '%' . $this->searchItems . '%');
+                $query->where(function ($q) {
+                    $q->where('code', 'like', '%' . $this->searchItems . '%')
+                        ->orWhere('name', 'like', '%' . $this->searchItems . '%');
+                });
             })
             ->limit(15)
             ->get();
