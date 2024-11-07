23a2138b56d103b44db039ee6c6f3b48357b09b3
Vetenskap <vetenskap2@gmail.com>
2024-11-07 09:42:34 +0500

    test

--- resources/views/components/marketPages/actions.blade.php
+++ resources/views/components/marketPages/actions.blade.php
@@ -8,34 +8,35 @@
                              wire:confirm="Вы действительно хотите пересчитать цены? Действие происходит в реальном времени, не перезагружайте страницу.">
                     Пересчитать цены
                 </flux:button>
-                <flux:button wire:click="testStocks"
-                             wire:confirm="Вы действительно хотите пересчитать остатки? Действие происходит в реальном времени, не перезагружайте страницу.">
-                    Пересчитать остатки
-                </flux:button>
                 <flux:button wire:click="nullStocks"
                              wire:confirm="Вы действительно хотите занулить кабинет? Действие нельзя будет отменить.">
                     Занулить кабинет
                 </flux:button>
             </div>
-            <div>
-                <flux:dropdown>
-                    <flux:button icon-trailing="chevron-down">Склады</flux:button>
+            <div class="flex gap-6">
+                <div>
+                    <flux:dropdown>
+                        <flux:button icon-trailing="chevron-down">Поставщики</flux:button>
 
-                    <flux:menu>
-                        @foreach($market->suppliers() as $supplier)
-                            <flux:menu.group :heading="$supplier->name">
-                                <flux:menu.checkbox
-                                    wire:model.live="testWarehouses.{{$supplier->getKey()}}.userWarehouses">Только ваши
-                                    склады
-                                </flux:menu.checkbox>
-                                @foreach($supplier->warehouses as $warehouse)
-                                    <flux:menu.checkbox
-                                        wire:model.live="testWarehouses.{{$supplier->getKey()}}.{{$warehouse->getKey()}}">{{$warehouse->name}}</flux:menu.checkbox>
-                                @endforeach
-                            </flux:menu.group>
-                        @endforeach
-                    </flux:menu>
-                </flux:dropdown>
+                        <flux:menu>
+                            @foreach($market->suppliers() as $supplier)
+                                    <flux:menu.checkbox>{{$supplier->name}}</flux:menu.checkbox>
+                            @endforeach
+                        </flux:menu>
+                    </flux:dropdown>
+                </div>
+                <flux:button wire:click="testStocks"
+                             wire:confirm="Вы действительно хотите пересчитать остатки? Действие происходит в реальном времени, не перезагружайте страницу.">
+                    Пересчитать остатки (почта)
+                </flux:button>
+                <flux:button wire:click="testStocks"
+                             wire:confirm="Вы действительно хотите пересчитать остатки? Действие происходит в реальном времени, не перезагружайте страницу.">
+                    Пересчитать остатки (апи)
+                </flux:button>
+                <flux:button wire:click="testStocks"
+                             wire:confirm="Вы действительно хотите пересчитать остатки? Действие происходит в реальном времени, не перезагружайте страницу.">
+                    Пересчитать остатки (только мои склады)
+                </flux:button>
             </div>
         </flux:card>
     </x-blocks.main-block>
