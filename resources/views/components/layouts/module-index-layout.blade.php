d8bbd9c0e2d289b3bf69af77d8f534c20caafaae
Vetenskap <vetenskap2@gmail.com>
2024-11-15 09:33:42 +0500

    test

--- resources/views/components/layouts/module-index-layout.blade.php
+++ resources/views/components/layouts/module-index-layout.blade.php
@@ -8,6 +8,7 @@
                 <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg p-6 space-y-6">
                     @foreach($modules as $module)
                         @dump($module)
+                        @dump(config(\Illuminate\Support\Str::lower($module->name)))
                         <flux:card>
                             <div class="flex justify-between">
                                 <a href="{{route(config(\Illuminate\Support\Str::lower($module->name) . ".main_route"))}}"
@@ -17,7 +18,7 @@
                                 <div>
                                     <flux:separator vertical/>
                                     <flux:switch
-                                        wire:model.live="changeOpen.{{$module->id}}"
+                                            wire:model.live="changeOpen.{{$module->id}}"
                                     />
                                 </div>
                             </div>
