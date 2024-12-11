2d8f9593b18094f44be6ce482471cff77727e043
Vetenskap <vetenskap2@gmail.com>
2024-12-11 21:47:39 +0500

    FIX!!!

--- Modules/SamsonApi/app/Jobs/SamsonUserProcess.php
+++ Modules/SamsonApi/app/Jobs/SamsonUserProcess.php
@@ -19,7 +19,7 @@ class SamsonUserProcess implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public int $tries = 2;
+    public int $tries = 1;
     /**
      * Create a new job instance.
      */
