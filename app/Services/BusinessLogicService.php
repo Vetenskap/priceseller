977564ee249e3a46e8fe7332f7a26ddec85f5ea1
Vetenskap <vetenskap2@gmail.com>
2024-12-12 10:19:55 +0500

    enabled supplier unload without price

--- app/Services/BusinessLogicService.php
+++ app/Services/BusinessLogicService.php
@@ -8,6 +8,7 @@ use App\Models\Supplier;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class BusinessLogicService
 {
@@ -23,14 +24,22 @@ class BusinessLogicService
 
         $time = now()->format('i');
 
-//        if ($time === "00") {
-//            Supplier::where('open', true)
-//                ->where('unload_without_price', true)
-//                ->chunk(5, function (Collection $suppliers) {
-//                    $suppliers->filter(fn (Supplier $supplier) => $supplier->user->isSub() || $supplier->user->isAdmin())->each(function (Supplier $supplier) {
-//                        UnloadOnTime::dispatch($supplier);
-//                    });
-//                });
-//        }
+        if ($time === "30") {
+            $offset = Cache::get('supplier-unload_without_price_offset', 0);
+            if ($offset > Supplier::where('open', true)->where('unload_without_price', true)->count()) {
+                $offset = 0;
+            }
+            Supplier::where('open', true)
+                ->where('unload_without_price', true)
+                ->offset($offset)
+                ->limit(2)
+                ->get()
+                ->each(function (Collection $suppliers) {
+                    $suppliers->filter(fn (Supplier $supplier) => $supplier->user->isSub() || $supplier->user->isAdmin())->each(function (Supplier $supplier) {
+                        UnloadOnTime::dispatch($supplier);
+                    });
+                });
+            Cache::set('supplier-unload_without_price_offset', $offset + 3);
+        }
     }
 }
